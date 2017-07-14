6071c5911c305b82163b7c5d9f482fe2017ef120
gandhichowdary <gandhi@maropost.com>
2017-07-14 17:30:09 +0530

    Update MaropostEmailContactsPage.java
    Adding Contact Pages

--- MaropostTestingProject/src/test/java/pages/MaropostEmailContactsPage.java
+++ MaropostTestingProject/src/test/java/pages/MaropostEmailContactsPage.java
@@ -3,6 +3,7 @@ package pages;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -30,7 +31,6 @@ public class MaropostEmailContactsPage {
 
         this.driver = driver;
         
-        Thread.sleep(3000);  
     }
     
     //Set user name in text box
@@ -70,35 +70,328 @@ public class MaropostEmailContactsPage {
         //Fill user name
 
         this.setUserEmail(strUserName);
-        
-        Thread.sleep(3000);
-        
+       
         //Fill password
 
         this.setUserPassword(strPasword);
 
-        Thread.sleep(3000);
-        
         //Click Login button
 
         this.clickLogin(); 
         
-        Thread.sleep(10000);
-       
     }
     
     public void clickNewContactsListOption() throws InterruptedException{     
         
-    	WebElement element = driver.findElement(By.xpath("//*[@id=\"header\"]/div/ul/li[4]/a"));
+    	WebElement newList = driver.findElement(By.xpath("//*[@id=\"main-container\"]/div[2]/div/div[2]/div/a"));
+       
+        newList.click();
+    
+        Thread.sleep(2000);
+		
+    	driver.switchTo().activeElement();
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement listName = driver.findElement(By.id("list_name"));
+        
+        listName.sendKeys("Automation_List");
+    	
+    	Thread.sleep(2000);
+    	
+    	JavascriptExecutor jse = (JavascriptExecutor) driver;
+    	
+        jse.executeScript("window.scrollBy(0,500)", "");
+    	
+    	driver.findElement(By.name("commit")).click();
+    	
+    	Thread.sleep(3000);
+    }
+
+    public void clickNewContactsFieldOption() throws InterruptedException{     
+        
+    	WebElement contacts = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[1]/ul/li[2]/a"));
+       
+        contacts.click();
+    
+        Thread.sleep(2000);
+		
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/i"));
         
         Actions action = new Actions(driver);
  
         action.moveToElement(element).build().perform();
- 
-        WebElement menuOption = driver.findElement(By.linkText("New List"));
         
-        menuOption.click();
+    	WebElement newField = driver.findElement(By.linkText("New Field"));
+
+    	newField.click();
+    	
+    	Thread.sleep(2000);
+    	
+    	driver.switchTo().activeElement();
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement fieldName = driver.findElement(By.id("custom_field_name"));
+        
+        fieldName.sendKeys("Automation_Field");
+    	
+    	Thread.sleep(2000);
+    	
+    	driver.findElement(By.name("commit")).click();
+    	
+    	Thread.sleep(3000);
+    }
+
+    public void clickNewContactsTagOption() throws InterruptedException{     
+        
+    	WebElement contacts = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[1]/ul/li[2]/a"));
+       
+        contacts.click();
     
+        Thread.sleep(2000);
+		
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/i"));
+        
+        Actions action = new Actions(driver);
+ 
+        action.moveToElement(element).build().perform();
+        
+    	WebElement newField = driver.findElement(By.linkText("New Tag"));
+
+    	newField.click();
+    	
+    	Thread.sleep(2000);
+    	
+    	driver.switchTo().activeElement();
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement tagName = driver.findElement(By.id("tag_name"));
+        
+        tagName.sendKeys("Automation_Tag");
+    	
+    	Thread.sleep(2000);
+    	
+    	driver.findElement(By.name("commit")).click();
+    	
+    	Thread.sleep(3000);
+    }
+
+    public void clickNewContactsBrandOption() throws InterruptedException{     
+        
+    	WebElement contacts = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[1]/ul/li[2]/a"));
+       
+        contacts.click();
+    
+        Thread.sleep(2000);
+		
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/i"));
+        
+        Actions action = new Actions(driver);
+ 
+        action.moveToElement(element).build().perform();
+        
+    	WebElement newField = driver.findElement(By.linkText("New Brand"));
+
+    	newField.click();
+    	
+    	Thread.sleep(2000);
+    	
+    	driver.switchTo().activeElement();
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement brandName = driver.findElement(By.id("brand_name"));
+        
+        brandName.sendKeys("Automation_Brand");
+    	
+    	Thread.sleep(2000);
+    	
+    	driver.findElement(By.name("commit")).click();
+    	
+    	Thread.sleep(3000);
+    }
+
+    public void clickNewContactsSecureListOption() throws InterruptedException{     
+        
+    	WebElement contacts = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[1]/ul/li[2]/a"));
+       
+        contacts.click();
+    
+        Thread.sleep(2000);
+		
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/i"));
+        
+        Actions action = new Actions(driver);
+ 
+        action.moveToElement(element).build().perform();
+        
+    	WebElement newField = driver.findElement(By.linkText("New Secure List"));
+
+    	newField.click();
+    	
+    	Thread.sleep(2000);
+    	
+    	driver.switchTo().activeElement();
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement secureListName = driver.findElement(By.id("secure_list_name"));
+        
+        secureListName.sendKeys("Automation_Secure_List");
+    	
+    	Thread.sleep(2000);
+    	
+    	driver.findElement(By.name("commit")).click();
+    	
+    	Thread.sleep(3000);
+    }
+
+    public void clickNewContactsTableOption() throws InterruptedException{     
+        
+    	WebElement contacts = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[1]/ul/li[2]/a"));
+       
+        contacts.click();
+    
+        Thread.sleep(2000);
+		
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/i"));
+        
+        Actions action = new Actions(driver);
+ 
+        action.moveToElement(element).build().perform();
+        
+    	WebElement newTable = driver.findElement(By.linkText("New Table"));
+
+    	newTable.click();
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement tableName = driver.findElement(By.id("relational_table_name"));
+        
+        tableName.sendKeys("automationtable");
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement name = driver.findElement(By.xpath("//*[@id=\"relational_table_relational_columns_attributes_0_name\"]"));
+        
+        name.sendKeys("automationtable");
+        
+        WebElement primaryKeyCheckbox = driver.findElement(By.xpath("//*[@id=\"relational_table_relational_columns_attributes_0_primary_key\"]"));
+        
+        primaryKeyCheckbox.click();
+        
+    	driver.findElement(By.name("commit")).click();
+    	
+    	Thread.sleep(3000);
+    }
+
+    public void clickNewContactsSQLQueryOption() throws InterruptedException{     
+        
+    	WebElement contacts = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[1]/ul/li[2]/a"));
+       
+        contacts.click();
+    
+        Thread.sleep(2000);
+		
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/i"));
+        
+        Actions action = new Actions(driver);
+ 
+        action.moveToElement(element).build().perform();
+        
+    	WebElement newSQLQuery = driver.findElement(By.linkText("New SQL Query"));
+
+    	newSQLQuery.click();
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement sqlQueryName = driver.findElement(By.id("relational_query_name"));
+        
+        sqlQueryName.sendKeys("automationsql");
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement sqlQuery = driver.findElement(By.id("relational_query_query"));
+        
+        sqlQuery.sendKeys("SELECT Email,Subject From demo_123");
+        
+        WebElement primaryKeyCheckbox = driver.findElement(By.xpath("//*[@id=\"relational_query_target_ids_\"]"));
+        
+        primaryKeyCheckbox.click();
+        
+    	driver.findElement(By.name("commit")).click();
+    	
+    	Thread.sleep(3000);
+    }
+
+    public void clickNewContactsNewPreferencePageOption() throws InterruptedException{     
+        
+    	WebElement contacts = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[1]/ul/li[2]/a"));
+       
+        contacts.click();
+    
+        Thread.sleep(2000);
+		
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/i"));
+        
+        Actions action = new Actions(driver);
+ 
+        action.moveToElement(element).build().perform();
+        
+    	WebElement newPreferencePage = driver.findElement(By.linkText("New Preference Page"));
+
+    	newPreferencePage.click();
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement preferencePageName = driver.findElement(By.id("preference_page_name"));
+        
+    	preferencePageName.sendKeys("automationpreferencepage");
+    	
+    	Thread.sleep(2000);
+    	
+    	JavascriptExecutor jse = (JavascriptExecutor) driver;
+    	
+        jse.executeScript("window.scrollBy(0,500)", "");
+    	
+    	driver.findElement(By.name("commit")).click();
+    	
+    	Thread.sleep(3000);
+    }
+
+    public void clickEditContactsListOption() throws InterruptedException{     
+        
+    	WebElement contacts = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[1]/ul/li[2]/a"));
+        
+        contacts.click();
+    
+        Thread.sleep(2000);
+	
+    	WebElement editList = driver.findElement(By.cssSelector("a[title=\"Edit list.\"]"));
+    	
+    	editList.click();
+    	
+    	driver.switchTo().activeElement();
+    	
+    	Thread.sleep(2000);
+    	
+    	WebElement editPage = driver.findElement(By.id("list_description"));
+        
+    	editPage.sendKeys("editing the list");
+    	
+    	Thread.sleep(2000);
+    	
+    	JavascriptExecutor jse = (JavascriptExecutor) driver;
+    	
+        jse.executeScript("window.scrollBy(0,500)", "");
+    	
+    	driver.findElement(By.name("commit")).click();
+    	
+    	Thread.sleep(3000);
+    	
     }
 
 }
+
